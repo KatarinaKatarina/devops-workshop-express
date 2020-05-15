8812bf0d53ac0eaa46e77b7ab9fc90cb6c783c14
katarinakacha90@yahoo.com <katarinakacha90@yahoo.com>
2020-05-15 21:07:21 +0200

    Finish workshop part 1

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
+// Import Express to be used as a web framework
+// and Mongoose to connect to a MongoDB service.
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = 8081;// Can be any port, but it has to match the Docker exposed ports.
+const HOST = '0.0.0.0';// Localhost
+const MONGO_URL = 'mongodb://mongo/test-express';// Url to the mongo container and test-express database.
+//taj mongo je definisan u servisima u docker-compose.yaml
+//ovo test-express je ime baze i mozemo dati kako zelimo
+
+mongoose.connect(MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    const app = express(); //kreiramo web app
+
+    app.get('/', (req, res) => res.send("Hello world!! test"));
+
+    app.listen(PORT, HOST); //startuje app
+
+    console.log(`Running on http://${HOST}:${PORT}`);
+  })
+  .catch(console.error);
+
 // // Import Express to be used as a web framework
 // // and Mongoose to connect to a MongoDB service.
 // const express = require('express');
@@ -13,28 +36,3 @@
 // app.listen(PORT, HOST);
 
 // console.log(`Running on http://${HOST}:${PORT}`);
-  
-  
-
-// Import Express to be used as a web framework
-// and Mongoose to connect to a MongoDB service.
-const express = require('express');
-const mongoose = require('mongoose');
-
-const PORT = 8081;// Can be any port, but it has to match the Docker exposed ports.
-const HOST = '0.0.0.0';// Localhost
-const MONGO_URL = 'mongodb://mongo/test-express';// Url to the mongo container and test-express database.
-//taj mongo je definisan u servisima u docker-compose.yaml
-//ovo test-express je ime baze i mozemo dati kako zelimo
-
-mongoose.connect(MONGO_URL, { useNewUrlParser: true })
-  .then(() => {
-    const app = express();
-
-    app.get('/', (req, res) => res.send("Hello world!! test"));
-
-    app.listen(PORT, HOST);
-
-    console.log(`Running on http://${HOST}:${PORT}`);
-  })
-  .catch(console.error);
